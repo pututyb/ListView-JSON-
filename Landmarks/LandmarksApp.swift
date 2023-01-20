42810d42065e2d6b6433438db4f757c8ec6a3b96
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-20 16:57:22 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Putut Yusri Bahtiar on 20/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
